894b704f06642ed603a4c6ca116c93c03de141f0
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-05 12:45:53 -0600

    added nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,24 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseSelectComponent } from './components/course-select/course-select.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseSelectComponent
+    CourseSelectComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
