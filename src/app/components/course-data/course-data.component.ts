b582588e4b3bafec9cce63d9260da25f59f05672
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-06 11:09:20 -0600

    pushing after class

--- src/app/components/course-data/course-data.component.ts
+++ src/app/components/course-data/course-data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseDataService } from '../../services/course-data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-data',
