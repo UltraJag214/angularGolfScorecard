e37d0160b7cf398ee79b7465c65090b30234e6f5
UltraJag214 <hayden.gibbons8920@stu.mtec.edu>
2021-04-06 11:33:16 -0600

    basic setup for the course data, will add API

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
@@ -11,12 +14,18 @@ import { MatCardModule } from '@angular/material/card';
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
